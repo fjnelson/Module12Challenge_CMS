dbb657d80b6d34f490edb5675fd9b6b421429b0a
fjnelson <97076779+fjnelson@users.noreply.github.com>
2023-01-29 10:10:44 -0800

    removing port

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,6 @@ const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
     host: "localhost",
-    port: PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
