2b28a9b703c1f705ee9ba98403416b659219e2f3
fjnelson <97076779+fjnelson@users.noreply.github.com>
2023-01-29 13:40:35 -0800

    updating seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -12,24 +12,24 @@ INSERT INTO role
     (title, salary, department_id)
 VALUES
     ('Customer Success Manager', 100000, 4),
-    ('Support Technician', 50000, 1),
-    ('Engineer', 140000, 2),
-    ('Support Administrator', 65000, 1),
-    ('VP Engineering', 195000, 2),
-    ('Support Lead', 100000, 1),
-    ('Account Manager', 110000, 3),
-    ('VP Sales', 200000, 3);
+    ('Support Technician', 50000, 2),
+    ('Engineer', 140000, 3),
+    ('Support Administrator', 65000, 2),
+    ('VP Engineering', 195000, 3),
+    ('Support Lead', 100000, 2),
+    ('Account Manager', 110000, 1),
+    ('VP Sales', 200000, 1);
 
 INSERT INTO employee
     (first_name, last_name, role_id, manager_id)
 VALUES
-    ('Emma', 'Smith', 1, NULL),
-    ('Michael', 'Johnson', 2, 1),
-    ('Olivia', 'Williams', 3, NULL),
-    ('Ethan', 'Jones', 4, 3),
-    ('Isabella', 'Brown', 4, 3),
+    ('Emma', 'Smith', 1, 9),
+    ('Michael', 'Johnson', 2, 7),
+    ('Olivia', 'Williams', 3, 6),
+    ('Ethan', 'Jones', 4, 7),
+    ('Isabella', 'Brown', 4, 7),
     ('Benjamin', 'Garcia', 5, NULL),
-    ('Ava', 'Miller', 6, 5),
-    ('William', 'Davis', 6, 5),
-    ('Sophia', 'Rodriguez', 7, NULL),
-    ('James', 'Martinez', 7, 8);
+    ('Ava', 'Miller', 6, 6),
+    ('William', 'Davis', 7, 9),
+    ('Sophia', 'Rodriguez', 8, NULL),
+    ('James', 'Martinez', 7, 9);
